5e4079b3bfe8fae4d7bcdf620ef8bd51a69040cc
stossthegreat <daazawaaza@proton.me>
2025-10-04 20:37:56 +0100

    Update MainApplication.kt

--- android/app/src/main/kotlin/com/example/drillos/MainApplication.kt
+++ android/app/src/main/kotlin/com/example/drillos/MainApplication.kt
@@ -1,5 +1,12 @@
 package com.example.drillos
 
+import android.app.Application
+import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 
-class MainApplication : MultiDexApplication()
+class MainApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        // You can initialize SDKs here if needed
+    }
+}
