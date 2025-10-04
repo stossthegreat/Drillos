83c544c53d3bf6c3d78bc5638fa27d677d9e6ee4
stossthegreat <daazawaaza@proton.me>
2025-10-04 18:48:45 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,39 +1,37 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id "com.android.application"
+    id "org.jetbrains.kotlin.android"
+    id "dev.flutter.flutter-gradle-plugin"
 }
 
 android {
-    namespace = "com.drillos.app"
-    compileSdk = 34
+    namespace "com.drillos.app"
+    compileSdk 34
 
     defaultConfig {
-        applicationId = "com.drillos.app"
-        minSdk = 23
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-        multiDexEnabled = true
+        applicationId "com.drillos.app"
+        minSdk 23
+        targetSdk 34
+        versionCode 1
+        versionName "1.0.0"
+        multiDexEnabled true
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            signingConfig = signingConfigs.getByName("debug")
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            shrinkResources true
+            minifyEnabled true
+            signingConfig signingConfigs.debug
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
         debug {
-            isMinifyEnabled = false
+            minifyEnabled false
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
 
     kotlinOptions {
@@ -41,10 +39,17 @@ android {
     }
 
     packaging {
-        resources.excludes.addAll(listOf("META-INF/LICENSE*", "META-INF/NOTICE*"))
+        resources.excludes += [
+            "META-INF/LICENSE*",
+            "META-INF/NOTICE*"
+        ]
     }
 }
 
+flutter {
+    source "../.."
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24")
+    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24"
 }
